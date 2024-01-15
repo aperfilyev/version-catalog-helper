b598a5f2fb8c6a2c20efb6273dad5927ca848751
GitHub Action <action@github.com>
2024-01-15 05:35:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/aperfilyev/versioncataloghelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aperfilyev/versioncataloghelper/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.aperfilyev.versioncataloghelper.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aperfilyev.versioncataloghelper.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/aperfilyev/versioncataloghelper/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/aperfilyev/versioncataloghelper/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.aperfilyev.versioncataloghelper.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.aperfilyev.versioncataloghelper.MyBundle
+import com.github.aperfilyev.versioncataloghelper.services.MyProjectService
 import javax.swing.JButton
 
 
